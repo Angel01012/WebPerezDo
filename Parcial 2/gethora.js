647f03722378bfd1cff81bbac3c306e928c28d4b
Angel01012 <dominguezangel1012@gmail.com>
2022-11-08 11:17:31 -0600

    Unidad 3

--- Parcial 2/gethora.js
+++
@@ -1,6 +0,0 @@
-$('btnhorasincrona').click(function(){
-    var solicitud = new XMLHttpRequest();
-    solicitud.onload = function() {
-        document.getElementById('hora').valu
-    }
-})
---
+++ Parcial 3/Modulos/modulo.js
@@ -0,0 +1,6 @@
+export function mayusculas (cad){
+    return cad.toUpperCase();
+}
+export function longuitud (cad){
+    return cad.length();
+}
