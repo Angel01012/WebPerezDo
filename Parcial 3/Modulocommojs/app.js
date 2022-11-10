32ddcfa8db3d7caee9fae22e20712b8db3f62bc5
Angel01012 <dominguezangel1012@gmail.com>
2022-11-10 09:09:05 -0600

    ModuloCommonJs

---
+++ Parcial 3/Modulocommojs/app.js
@@ -0,0 +1,9 @@
+const modulo = require("./modulo.js");
+console.log(modulo.cuadrado(8));
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    Text : "I'am a module",
+    e : "OO",
+    t : "UU"
+}));
---
+++ Parcial 3/Modulocommojs/modulo.js
@@ -0,0 +1,4 @@
+const cuadrado = (valor) => {return valor*valor;}
+console.log(module);
+console.log(cuadrado(8));
+exports.cuadrado=cuadrado;
