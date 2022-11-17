0205c67362d37dad48fb17ee0113ba88bb5582de
Angel01012 <dominguezangel1012@gmail.com>
2022-11-17 12:15:40 -0600

    formulario

---
+++ Parcial 3/Formulario/formulario.php
@@ -0,0 +1,10 @@
+<?php
+$regisro["Nombre"]="Angel";
+$regisro["Usuario"]="Angel001012";
+$regisro["Telefono"]="8672809248";
+$regisro["Edad"]="21";
+$regisro["Correo"]="perezangel2015@gmail.com";
+$regisro["Contraseña"]="Password";
+$regisro["ConfirmarContra"]="Password";
+echo json_encode ($regisro);
+?>
