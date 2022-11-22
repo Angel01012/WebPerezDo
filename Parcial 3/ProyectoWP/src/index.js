1877f1255186c3615d4295c4936db95f1e9a1117
Angel01012 <dominguezangel1012@gmail.com>
2022-11-22 13:39:30 -0600

    WebPack

---
+++ Parcial 3/ProyectoWP/src/index.js
@@ -0,0 +1,3 @@
+import { cuadrado } from "./modulo";
+console.log(cuadrado(2));
+console.log("Saludos");
--- Parcial 3/ProyectoWP/webpack.config.js
+++ Parcial 3/ProyectoWP/webpack.config.js
@@ -2,8 +2,21 @@ console.log(module);
 console.log(__dirname);
 console.log(__filename);
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: ''
+    entry: './src/index.js',
+output: {
+filename: 'bundle.js',
+path: path.resolve(__dirname,'dist')
+},
+mode: "production",
+plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+    })n
+]
 }
