07d714bfe92e34ac11d549df4967e18374c1cd7c
Angel01012 <dominguezangel1012@gmail.com>
2022-11-22 14:04:58 -0600

    f

--- Parcial 3/Formulario/mysqli/conexion.php
+++ Parcial 3/Formulario/mysqli/conexion.php
@@ -1,12 +1,12 @@
 <?php
-$id = $_POST['Angel001012'];
+$id = $_POST['Usuario'];
 $servidor="localhost";
 $basedatos="Web";
 $usuario="root";
 $password="162460132-2";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("no se pudo conectar al servidor");
-$consulta = "select * from login where Usuario = $id";
+$consulta = "select * from login where Usuario = '$id'";
 $registro =mysqli_query($con,$consulta) or die("No se puso hacer la consulta");
 
 $result = mysqli_fetch_array($registro,MYSQLI_ASSOC);
@@ -14,6 +14,4 @@ $result = mysqli_fetch_array($registro,MYSQLI_ASSOC);
 mysqli_close($con);
 echo json_encode($result);
 //var_dump($result);
-
-
 ?>
